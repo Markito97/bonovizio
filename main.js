fe277fe631ad693321ab866b5b35ad5d4a1b7787
Markito97 <jetblade0@gmail.com>
2023-04-02 20:28:40 +0300

    промо + слайдер

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 import { Navigation, Pagination } from "swiper";
 import "./src/scss/index.scss";
-
 import Swiper from "swiper/bundle";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
 
-import "swiper/css/bundle";
+const menu = document.querySelector(".nav");
+const burgerBtn = document.querySelector(".burger__menu");
+
+burgerBtn.addEventListener("click", (e) => {
+  burgerBtn.classList.toggle("active");
+  menu.classList.toggle("open");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    menu.classList.remove("open");
+    burgerBtn.classList.remove("active");
+  }
+});
 
 const swiper = new Swiper(".swiper", {
+  slidesPerView: 2,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-custom-pagination",
+    paginationDistance: "30px",
     clickable: true,
   },
   navigation: {
