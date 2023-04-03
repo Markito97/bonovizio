cff712a851a2c66e86bc40d670d82ca952b5ea42
Markito97 <jetblade0@gmail.com>
2023-04-03 17:17:12 +0300

    staged version

--- main.js
+++ main.js
@@ -1,37 +1,71 @@
 import { Navigation, Pagination } from "swiper";
-import "./src/scss/index.scss";
+import anime from "animejs/lib/anime.es.js";
 import Swiper from "swiper/bundle";
+import "./src/scss/index.scss";
 import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
 
-const menu = document.querySelector(".nav");
-const burgerBtn = document.querySelector(".burger__menu");
+console.log(anime);
 
-burgerBtn.addEventListener("click", (e) => {
-  burgerBtn.classList.toggle("active");
-  menu.classList.toggle("open");
-});
+(function start() {
+  const menu = document.querySelector(".nav");
+  const burgerBtn = document.querySelector(".burger__menu");
+  const messageForm = document.querySelector(".message__form");
 
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 768) {
-    menu.classList.remove("open");
-    burgerBtn.classList.remove("active");
-  }
-});
+  const sendForm = (e) => {
+    e.preventDefault();
+  };
 
-const swiper = new Swiper(".swiper", {
-  slidesPerView: 2,
-  pagination: {
-    el: ".swiper-custom-pagination",
-    paginationDistance: "30px",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  slidesPerView: 2,
-  spaceBetween: 20,
-  modules: [Navigation, Pagination],
-});
+  messageForm.addEventListener("submit", sendForm);
+
+  let isOpen = false;
+
+  const handleMenu = () => {
+    burgerBtn.classList.toggle("active");
+    menu.classList.toggle("open");
+    isOpen = !isOpen;
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "scroll";
+    }
+  };
+
+  burgerBtn.addEventListener("click", handleMenu);
+
+  const desktopSwiper = new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      575: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 50,
+      },
+    },
+    pagination: {
+      el: ".swiper-custom-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next-unique",
+      prevEl: ".swiper-button-prev-unique",
+    },
+    modules: [Navigation, Pagination],
+  });
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 768) {
+      menu.classList.remove("open");
+      burgerBtn.classList.remove("active");
+    }
+  });
+})();
